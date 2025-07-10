3fa7c53040f85ba9210a21d441cc092354cb212d
Allan Marillier <ivan.marillier@tanium.com>
2025-07-09 20:54:38 -0500

    minor update setting icons on binaries

--- make.sh
+++ make.sh
@@ -4,3 +4,6 @@ GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -ldflags="-w -s" -o bin/jiggler-
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -ldflags="-w -s" -o bin/jiggler-MacOSAMD64
 GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC="x86_64-w64-mingw32-gcc" go build -ldflags="-w -s" -o bin/jiggler.exe #  -H windowsgui -r img2icons.rc" -o bin/jiggler.exe
 # GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -ldflags="-w -s" -o bin/jiggler-LinuxAMD64
+setIcon.sh KrankyBearBeret.png bin/jiggler-MacOSARM64
+setIcon.sh KrankyBearBeret.png bin/jiggler-MacOSAMD64
+setIcon.sh KrankyBearBeret.png bin/jiggler.exe
